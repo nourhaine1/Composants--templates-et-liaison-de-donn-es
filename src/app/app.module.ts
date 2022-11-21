be7fa3af7036aa58be3a404a1bd3f951e672cff7
nourhaine1 <ayadinourhaine@gmail.com>
2022-11-21 14:12:14 +0100

    Partie4:Two Way databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
-
+import { TwoWayComponent } from './two-way/two-way.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     InterpolationComponent,
     PropertyBindingComponent,
-    EventBindingComponent
+    EventBindingComponent,
+    TwoWayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/two-way/two-way.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way',
+  templateUrl: './two-way.component.html',
+  styleUrls: ['./two-way.component.css']
+})
+export class TwoWayComponent implements OnInit {
+  message:string = "Génie Logiciel";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onAffiche(){
+    return this.message;
+  }
+}
